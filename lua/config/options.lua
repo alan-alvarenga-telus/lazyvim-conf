b59d679fd4d04543bca7f941b48e15705b54865a
Alan Alvarenga <alan.alvarengamejia1@telus.com>
2024-09-27 09:43:54 -0600

    updating default options

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,21 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+vim.scriptencoding = "utf-8"
+vim.opt.encoding = "utf-8"
+vim.opt.fileencoding = "utf-8"
+vim.opt.title = true
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+vim.opt.hlsearch = true
+vim.opt.expandtab = true
+vim.opt.scrolloff = 10
+vim.opt.breakindent = true
+vim.opt.wrap = false
+vim.opt.backspace = { "start", "eol", "indent" }
+vim.opt.path:append({ "**" })
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+vim.opt.splitkeep = "cursor"
+vim.opt.formatoptions:append({ "r" })
+vim.opt.mouse = ""
