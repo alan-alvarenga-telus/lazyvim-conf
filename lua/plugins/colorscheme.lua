235be4ea1a07adacfc11836a2a78ab834803a914
Alan Alvarenga <alan.alvarengamejia1@telus.com>
2025-03-10 10:23:46 -0600

    (chore): update colorschema

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,9 +1,13 @@
 return {
-  "loctvl842/monokai-pro.nvim",
   {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "monokai-pro",
-    },
+    "loctvl842/monokai-pro.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("monokai-pro").setup({
+        filter = "spectrum", -- Options: classic, octagon, pro, machine, ristretto, spectrum
+      })
+      vim.cmd("colorscheme monokai-pro")
+    end,
   },
 }
